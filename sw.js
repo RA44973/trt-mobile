2fe82bf7fa9f1bc09b047924815daa11712e3742
RA44973 <ra44973@gmail.com>
2026-07-22 12:41:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'trt-mobile-v1-5';
+const CACHE_NAME = 'trt-mobile-v1-5-1';
 const SHELL = [
   './',
   './index.html',
   './styles.css',
   './app.js',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
